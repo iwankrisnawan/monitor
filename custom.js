5f812d802357af7b29b951c2c29c63a6eba0146e
iwankrisnawan <iwankrisnawan03@gmail.com>
2020-12-28 11:48:40 +0800

    initial commit

--- custom.js
+++ custom.js
@@ -1,22 +1,18 @@
 function readApiThingSpeak(){
-	$("#suhu").html("");
-	$("#kelembaban").html("");
-	$.getJSON("https://api.thingspeak.com/channels/1269281/feeds.json?results=5", function(data){
+	$.getJSON("https://api.thingspeak.com/channels/1269281/feeds.json?results=10", function(data){
+		let lastData = data.feeds[data.feeds.length-1];
+		let dataUpdate = {suhu:'',kelembaban:''};
 		$.each(data.feeds, function(i){
-			$("#suhu").append(
-				`<li>
-					${data.feeds[i].created_at} <span class="pull-right"><b>${data.feeds[i].field1}</b> </span>
-				</li>`
-			);
-
-			$("#kelembaban").append(
-				`<li>
-					${data.feeds[i].created_at} <span class="pull-right"><b>${data.feeds[i].field2}</b> </span>
-				</li>`
-			);
+			dataUpdate['suhu'] = dataUpdate['suhu'] + `<li><i class="fa fa-clock-o"></i> ${moment(data.feeds[i].created_at).format("HH:mm:ss")} <span class="pull-right"><b>${data.feeds[i].field1.replace('.00','')}&deg</b> </span></li>`;
+			dataUpdate['kelembaban'] = dataUpdate['kelembaban'] + `<li><i class="fa fa-clock-o"></i> ${moment(data.feeds[i].created_at).format("HH:mm:ss")} <span class="pull-right"><b>${data.feeds[i].field2.replace('.00','')}&deg</b> </span></li>`;
 		});
+
+		$('#suhu_log').html(dataUpdate['suhu']);
+		$('#kelembaban_log').html(dataUpdate['kelembaban']);
+
+		$('#suhu_now').html(`${lastData.field1.replace('.00','')}&deg`);
+		$('#kelembaban_now').html(`${lastData.field2.replace('.00','')}&deg`);
 	});
-	console.log('yes');
 
 	setTimeout(readApiThingSpeak, 5000);
 }
